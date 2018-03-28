ff5ecc4085565e01e6b9db8db4faa3076dfff993
VietDung <vietdung@ringier.com.vn>
2018-03-28 18:43:38 +0700

    a

--- wp-config.php
+++ wp-config.php
@@ -100,6 +100,6 @@ $table_prefix  = 'v_';
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
-
+define('FS_METHOD','direct');
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
--- wp-content/themes/onepress/404.php
+++ wp-content/themes/onepress/404.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 		<div class="page-header">
 			<div class="container">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'onepress' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Xin lỗi trang bạn truy cập hiện không tìm thấy.', 'vaycaptoc.com' ); ?></h1>
 			</div>
 		</div>
 
@@ -23,12 +23,12 @@ get_header(); ?>
 					<section class="error-404 not-found">
 
 						<div class="page-content">
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'onepress' ); ?></p>
+							<p><?php esc_html_e( 'Hãy thử tìm kiếm với 1 số từ khóa liên quan !', 'vaycaptoc.com' ); ?></p>
 
 							<?php get_search_form(); ?>
 
 							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
+                        <div><img src="https://vaycaptoc.com/wp-content/uploads/2018/03/404.jpg" alt=""></div>
 							<?php if ( onepress_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 							<div class="widget widget_categories">
 								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'onepress' ); ?></h2>
@@ -46,13 +46,13 @@ get_header(); ?>
 							</div><!-- .widget -->
 							<?php endif; ?>
 
-							<?php
-								/* translators: %1$s: smiley */
-								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'onepress' ), convert_smilies( ':)' ) ) . '</p>';
-								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-							?>
-
-							<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+<!--							--><?php
+//								/* translators: %1$s: smiley */
+//								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'onepress' ), convert_smilies( ':)' ) ) . '</p>';
+//								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+//							?>
+<!---->
+<!--							--><?php //the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
 						</div><!-- .page-content -->
 					</section><!-- .error-404 -->
