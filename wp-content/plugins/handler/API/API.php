3b3127b1fa422ce3601c505075c37dc0268cf70e
VietDung <vietdung@ringier.com.vn>
2018-05-07 16:09:04 +0700

    a

--- wp-content/plugins/handler/API/API.php
+++ wp-content/plugins/handler/API/API.php
@@ -1,13 +1,13 @@
 <?php
 namespace VCT;
 
-use Theme_My_Login_Common;
+
 use WP_Error;
 use WP_REST_Controller;
 use WP_REST_Request;
 use WP_REST_Response;
 use WP_REST_Server;
-use WP_User;
+use WeDevs\ORM\Eloquent\Database as DB;
 
 
 class API extends WP_REST_Controller {
@@ -48,86 +48,11 @@ class API extends WP_REST_Controller {
     }
 
     public function get_guest(){
-        return json_encode('api-guest');
-    }
-    /**
-     * Get token by access token facebook
-     *
-     * @param WP_REST_Request $request Full details about the request.
-     *
-     * @return array|WP_Error|WP_REST_Response
-     */
-    public function get_token_by_facebook( $request ) {
-        if ( empty( $request['access_token'] ) ) {
-            return [ 'status' => 'error', 'code' => 'access_token_empty' ];
-        }
 
-        $url_get_me = 'https://graph.facebook.com/v2.12/me?access_token=' . $request['access_token'];
-        $result     = file_get_contents( $url_get_me );
-        if ( ! $result ) {
-            return [ 'status' => 'error', 'code' => 'access_token_invalid' ];
-        }
-        $result = json_decode( $result, 1 );
-
-
-        $users        = get_users( array( 'meta_key' => 'facebook_id', 'meta_value' => $result['id'] ) );
-        $current_user = null;
-        foreach ( $users as $user ) {
-            $current_user = $user->data;
-        }
-
-        if ( is_null( $current_user ) ) {
-            return [ 'status' => 'error', 'code' => 'rest_user_not_exists' ];
-        }
-
-        $user = get_user_by( 'login', $current_user->user_login );
-
-
-        /** If the authentication fails return a error*/
-        if ( is_wp_error( $user ) ) {
-            /** @var $user WP_Error*/
-            $error_code = $user->get_error_code();
-
-            return new WP_Error(
-                '[jwt_auth] ' . $error_code,
-                $user->get_error_message( $error_code ),
-                array(
-                    'status' => 403,
-                )
-            );
-        }
-
-        /** Valid credentials, the user exists create the according Token */
-        $issuedAt  = time();
-        $notBefore = apply_filters( 'jwt_auth_not_before', $issuedAt, $issuedAt );
-        $expire    = apply_filters( 'jwt_auth_expire', $issuedAt + ( DAY_IN_SECONDS * 7 ), $issuedAt );
-
-        $token      = array(
-            'iss'  => get_bloginfo( 'url' ),
-            'iat'  => $issuedAt,
-            'nbf'  => $notBefore,
-            'exp'  => $expire,
-            'data' => array(
-                'user' => array(
-                    'id' => $user->data->ID,
-                ),
-            ),
-        );
-        $secret_key = defined( 'JWT_AUTH_SECRET_KEY' ) ? JWT_AUTH_SECRET_KEY : false;
-        /** Let the user modify the token data before the sign. */
-        $token = JWT::encode( apply_filters( 'jwt_auth_token_before_sign', $token, $user ), $secret_key );
-
-        /** The token is signed, now create the object with no sensible user data to the client*/
-        $data = array(
-            'token'             => $token,
-            'user_email'        => $user->data->user_email,
-            'user_nicename'     => $user->data->user_nicename,
-            'user_display_name' => $user->data->display_name,
-        );
-
-        /** Let the user modify the data before send it back */
-        return apply_filters( 'jwt_auth_token_before_dispatch', $data, $user );
-
+        $DB = new DB();
+        $page = !empty($_GET['page']) ? $_GET['page'] : 1;
+        $dataGuest = $DB->table('guests')->orderByDesc('created_at')->paginate(50,['*'],'page',$page);
+        return json_encode($dataGuest);
     }
 
     /**
